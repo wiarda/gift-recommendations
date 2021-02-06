cda53a61d9150786a5dd813690debaf484772e41
Paul Wiarda <wiarda@gmail.com>
2021-02-06 08:48:35 -0800

    Catch errors on parsing strategies

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,22 +1,45 @@
-import cheerio from 'cheerio';
-import { Strategy } from './strategies/types';
+import cheerio from "cheerio";
+import { Strategy } from "./strategies/types";
 
-export type runStrategy<K> = (html: string, selector?: string) => K[]
-type loadStrategy<K> = (x: Strategy<K>) => runStrategy<K>
+export type ScrapedItem = {
+  selector?: string;
+  url?: string;
+};
 
-export const loadStrategy: loadStrategy<any> = strategy => (html, selector = "body") => {
-    const $ = cheerio.load(html);
-    const results = [];
+export type RunStrategy<K> = (html: string, selector?: string) => K[];
 
-    $(selector).each(function (idx, el) {
-        const result = {};
+type LoadStrategy<K> = (
+  x: Strategy<K>,
+  condition?: (
+    idx: number,
+    el: cheerio.Element,
+    selection: cheerio.Cheerio
+  ) => boolean
+) => RunStrategy<K>;
 
-        for (const prop in strategy) {
-            result[prop] = strategy[prop]($, el);
-        }
-        
-        results.push(result);
-    })
+export const loadStrategy: LoadStrategy<any> = (strategy, condition) => (
+  html
+) => {
+  const $ = cheerio.load(html);
+  const results = [];
+  const selector = strategy.selector(null, null);
 
-    return results;
-}
+  $(selector).each(function (idx, el) {
+    if (condition !== undefined && !condition(idx, el, $(selector))) return;
+
+    const result = {};
+
+    for (const prop in strategy) {
+      try {
+        if (prop === "selector") continue;
+        result[prop] = strategy[prop]($, el);
+      } catch (e) {
+        result[prop] = "Error";
+      }
+    }
+
+    results.push(result);
+  });
+
+  return results;
+};
--- scraper/strategies/types.d.ts
+++ scraper/strategies/types.d.ts
@@ -1,6 +1,10 @@
 type StrategyMethod<T> = ($: cheerio.Root, domRoot: cheerio.Element) => T;
 
 export type Strategy<T> = {
-    [key in keyof T]: StrategyMethod<T[key]>
+  [key in keyof T]: StrategyMethod<T[key]>;
 };
 
+export type ScrapedItem = {
+  selector?: string;
+  url?: string;
+};
